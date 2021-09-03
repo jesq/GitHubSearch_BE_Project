ad8d54e2ce35b25bfaf06f3ab7de51a72e3197e3
Geo3777 <87963537+Geo3777@users.noreply.github.com>
2021-09-03 19:19:24 +0300

    fetching data from api

--- GitHubSearchWebApp/wwwroot/js/search.js
+++ GitHubSearchWebApp/wwwroot/js/search.js
@@ -47,5 +47,13 @@ class UI {
 }
 
 document.querySelector('#submitBtn').addEventListener('click', (e) => {
-    UI.displayResults();
+    e.preventDefault;
+    var search = document.getElementById("search_bar").val;
+    //var originalRepo = search.split(' ').join(' ');
+
+    fetch("https://localhost:5001/api/gitrepository/" + search)
+        .then((result) => result.json())
+        .then((data) => {
+            console.log(data)
+        })
 })
--- GitHubSearchWebApp/wwwroot/js/site.js
+++ GitHubSearchWebApp/wwwroot/js/site.js
@@ -1,16 +1,26 @@
-﻿var submitt = document.getElementById("mySubmit");
-submitt.addEventListener('submit', function (e) {
+﻿////var submitt = document.getElementById("mySubmit");
+////submitt.addEventListener('submit', function (e) {
+////    e.preventDefault;
+////    var search = document.getElementById("search_bar").val;
+////    var originalRepo = search.split(' ').join(' ');
+
+////    fetch("https://localhost:5001/api/gitrepository/" + originalRepo)
+////        .then((result) => result.json())
+////        .then((data) => {
+////            console.log(data)
+////        })
+////})
+
+document.querySelector('#submitBtn').addEventListener('click', (e) => {
     e.preventDefault;
     var search = document.getElementById("search_bar").val;
-    var originalRepo = search.split(' ').join(' ');
+    //var originalRepo = search.split(' ').join(' ');
 
-    fetch("https://localhost:5001/api/gitrepository/" + originalRepo)
+    fetch("https://localhost:5001/api/gitrepository/" + search)
         .then((result) => result.json())
         .then((data) => {
             console.log(data)
         })
 })
-
-
 
 
