f5eb0653bbb68923c783c12ed7f0f0caeb4c81b2
Cosmin Jescu <57216949+jesq@users.noreply.github.com>
2021-09-03 18:22:22 +0300

    Created search result template

--- GitHubSearchWebApp/wwwroot/js/search.js
+++ GitHubSearchWebApp/wwwroot/js/search.js
@@ -1,1 +1,25 @@
-﻿
+﻿class UI {
+    static addSearchResultToList() {
+        const list = document.querySelector('#search-results-list');
+        const button = document.createElement('button');
+        button.classList.add('list-group-item');
+        button.classList.add('list-group-action');
+        button.innerHTML = `
+            <ul class="list-group list-group-horizontal align-items-center justify-content-center">
+                <li class="list-group-item border-0">
+                    <img src="images/default_avatar.png" width="50" height="50" class="rounded-circle" alt="Cinque Terre">
+                </li>
+                <li class="list-group-item border-0">
+                    UserName
+                </li>
+                <li class="list-group-item border-0">
+                    RepoName
+                </li>
+            </ul>`
+        list.appendChild(button);
+    }
+}
+
+document.querySelector('#submitBtn').addEventListener('click', (e) => {
+    UI.addSearchResultToList();
+})
