b820b249ee72a194448f55c8e7a3293df72e7b38
Cosmin Jescu <57216949+jesq@users.noreply.github.com>
2021-09-05 15:28:29 +0300

    Search results are now displayed

--- GitHubSearchWebApp/wwwroot/js/search.js
+++ GitHubSearchWebApp/wwwroot/js/search.js
@@ -1,59 +1,60 @@
-﻿
-class Result {
-    constructor(profileImage, userName, repoName) {
-        this.profileImage = profileImage;
-        this.userName = userName;
-        this.repoName = repoName;
-    }
-}
-
-
+﻿var results = [];
 
 class UI {
-    static displayResults() {
-        let result1 = new Result("images/default_avatar.png", "cosmin", "repo");
-        let result2 = new Result("images/default_avatar.png", "sofia", "repoTest1");
-        let result3 = new Result("images/default_avatar.png", "george", "repoTest2");
-        let result4 = new Result("images/default_avatar.png", "stefan", "repoooooo");
-        let result5 = new Result("images/default_avatar.png", "TEST", "repository");
-        const results = [];
-        results.push(result1);
-        results.push(result2);
-        results.push(result3);
-        results.push(result4);
-        results.push(result5);
-        results.forEach((result) => UI.addSearchResultToList(result));
+    static async displayResults(data) {
+        await Store.getResults(data);
+        console.log(results);
+        results.forEach((result) => {
+            console.log(result);
+            UI.addSearchResultToList(result)
+        });
     }
 
     static addSearchResultToList(result) {
         const list = document.querySelector('#search-results-list');
         const button = document.createElement('button');
+        button.setAttribute("onclick", `window.open("${ result.htmlUrl }", "_blank")`);
         button.classList.add('list-group-item');
         button.classList.add('list-group-action');
         button.innerHTML = `
             <ul class="list-group list-group-horizontal align-items-center justify-content-center">
                 <li class="list-group-item border-0 m-auto">
-                    <img src="${result.profileImage}" width="50" height="50" class="rounded-circle" alt="Cinque Terre">
+                    <img src="${result.owner.avatarUrl}" width="50" height="50" class="rounded-circle" alt="Cinque Terre">
                 </li>
                 <li class="list-group-item border-0 m-auto">
-                    ${result.userName}
+                    ${result.name}
                 </li>
                 <li class="list-group-item border-0 m-auto">
-                    ${result.repoName}
+                    ${result.owner.username}
                 </li>
             </ul>`
         list.appendChild(button);
     }
+
+    static resetResults() {
+        results.length = 0;
+        document.querySelector('#search-results-list').innerHTML = '';
+    }
+}
+
+class Store {
+    static getResults(data) {
+        data.forEach((result) => results.push(result));
+        return results;
+    }
+
 }
 
 document.querySelector('#submitBtn').addEventListener('click', (e) => {
     e.preventDefault;
+    UI.resetResults();
     var search = document.getElementById("search_bar").value;
     //var originalRepo = search.split(' ').join(' ');
     console.log(search)
     fetch("https://localhost:5001/api/gitrepository/" + search)
         .then((result) => result.json())
         .then((data) => {
-            console.log(data)
+            console.log(data);
+            UI.displayResults(data);
         })
 })
